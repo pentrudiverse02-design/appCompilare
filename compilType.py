5cfd551aa2d3ebc2c7d64fe8f2c94f87047b1db7
David <pentru.diverse.02@gmail.com>
2026-07-02 15:00:40 +0300

    am adaugat enum urile care o sa fie folosite peste tot

---
+++ compilType.py
@@ -0,0 +1,6 @@
+from enum import Enum
+
+class CompilationType(Enum):
+    RELEASE=1
+    DEBUG=2
+    LIBRARY=3
---
+++ sysArhitecture.py
@@ -0,0 +1,6 @@
+from enum import Enum
+
+class SystemArchitecture(Enum):
+    x86_64=1
+    arm64=2
+    aarch_64=3
