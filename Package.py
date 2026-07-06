ad978e18c6e6a7f6b91f1b8a4757236f5aaef4e8
David <pentru.diverse.02@gmail.com>
2026-07-06 16:50:57 +0300

    major improvment

---
+++ Package.py
@@ -0,0 +1,26 @@
+import asyncio
+import struct
+from calendar import week
+from enum import IntEnum
+
+
+class PackageType(IntEnum):
+    UPLOAD_ZIP = 1
+    GET_LOGS = 2
+    GET_STATUS = 3
+    GET_ERRORS = 4
+    RESULT = 5
+HEADER_FORMAT = "!BI" # ! = little endian , B = Byte ,  I = Integer
+HEADER_SIZE= struct.calcsize(HEADER_FORMAT)
+
+async def ReadPackage(reader: asyncio.StreamReader):
+    header = await reader.read(HEADER_SIZE)
+    type, lenght = struct.unpack(HEADER_FORMAT,header)
+    payload = await reader.readexactly(lenght)
+    return type, payload
+async def WritePackage(writer : asyncio.StreamWriter,
+                       messageType : PackageType,
+                       payload : bytes):
+    header = struct.pack(HEADER_FORMAT, int(messageType),len(payload))
+    writer.write(header + payload)
+    await writer.drain()
--- server.py
+++ server.py
@@ -8,25 +8,36 @@ class Server:
     server_port = 8008
     folderServer = ''
     maxConnections = 0
-    SERVER=0
+    SERVER=None
     def __init__(self):
         asyncio.run(self.CreateServer())
     async def CreateServer(self):
-        self.SERVER= await asyncio.start_server(self.ClientConnect,self.server_host,self.server_port)
-
+        self.SERVER = await asyncio.start_server(self.ClientHandle,self.server_host,self.server_port)
         addrs = ', '.join(str(sock.getsockname()) for sock in self.SERVER.sockets)
         print(f'Serving on {addrs}')
         async with self.SERVER:
             await self.SERVER.serve_forever()
 
+    async def ClientConnect(self,addr,reader:asyncio.StreamReader,writer:asyncio.StreamWriter):
+        data= await reader.read(1024)
+        data=data.decode('utf-8')
+        self.clients[addr]={reader,writer,data}
+        writer.write("ok".encode('utf-8'))
+        await writer.drain()
 
-
-    async def ClientConnect(self,reader,writer):
+    async def ClientHandle(self,reader:asyncio.StreamReader,writer:asyncio.StreamWriter):
+        if not self.clients.__contains__(writer.get_extra_info('peername')):
+            self.ClientConnect(writer.get_extra_info('peername'), reader,writer)
         data = await reader.read(1024)
         message = data.decode('utf-8')
         addr = writer.get_extra_info('peername')
         print(f"am primit {message} de la {addr}")
+        self.clients[addr]=message
         writer.write("ok".encode('utf-8'))
         await writer.drain()
 
-Server()
+while True:
+    a=Server()
+    print("introduceti ce doriti sa faceti cu urmatoarele coenxiuni:")
+    for i in a.clients:
+        print(f" client {i} ", end ='' )
