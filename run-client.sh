4e6eb5faf5f1347cb8fa02f9b931c7e8ac6bdda1
David <pentru.diverse.02@gmail.com>
2026-07-15 11:46:31 +0300

    ianinte de munca

--- run-client.sh
+++ run-client.sh
@@ -1,14 +1,34 @@
 #!/usr/bin/env bash
+
+GenRandNameFolder()
+{
+  nrCharsForName=10
+  tr -dc A-Za-z0-9 </dev/urandom | head -c ${nrCharsForName}
+}
 cd clientdir/ZipsToBorrow || exit 1
 zips=( *.zip )
-for zip in "${zips[@]}"; do
-  echo "$zip"
-done
+cd ..
+sysarhi=( x86_64 arm64 aarch_64 )
+compileFor=( RELEASE DEBUG LIBRARY )
+
+randZips=$(( $RANDOM % ${#zips[@]} ))
+#echo ${arr[$rand]}
+randSys=$[$RANDOM % ${#sysarhi[@]}]
+randComp=$[$RANDOM % ${#compileFor[@]}]
+
+#for zip in "${zips[@]}"; do
+#  echo "$zip"
+#done
 nrClients=2
 ls
-cd ..
-
 for ((i=0;i<nrClients;i++)); do
-  
-
+  vc=${compileFor[randComp]}
+  vs=${sysarhi[randSys]}
+  nume=${GenRandNameFolder}
+  mkdir ${nume}
+  for ((j=0;j<${randZips};j++)); do
+      cp ZipsToBorrow/${zips[randZips]} ${nume}
+  done
+  cd ..
+  python3 -m clientdir.client ${vc} ${vs} ${RANDOM}%2 ${nume}
 done
